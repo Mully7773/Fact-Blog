c06c0ce134693bee1c7ec164ef804c561ffd4157
Nick M <mully7773@gmail.com>
2022-12-14 10:54:07 -0600

    Begin rendering old code into JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import "./css/style.css";
+
+const App = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src="../img/logo.png" alt="Today I Learned Logo" />
+        <h1 className="header__heading">Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large shareBtn">Share a fact</button>
+    </header>
+  );
+};
 
 export default App;
