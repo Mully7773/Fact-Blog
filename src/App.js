071e6b4a09a05f20031ca306f45d0d74057518aa
Nick M <mully7773@gmail.com>
2022-12-14 11:36:06 -0600

    Add initial layout for react app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import AppContainer from "./components/AppContainer";
 import "./css/style.css";
 
 const App = () => {
-  return (
-    <header className="header">
-      <div className="header__logo">
-        <img src="../img/logo.png" alt="Today I Learned Logo" />
-        <h1 className="header__heading">Today I Learned</h1>
-      </div>
+  const appTitle = "Today I Learned";
 
-      <button className="btn btn-large shareBtn">Share a fact</button>
-    </header>
+  return (
+    <>
+      <AppContainer>
+        {/* HEADER */}
+        <header className="header">
+          <div className="header__logo">
+            <img src="../img/logo.png" alt="Today I Learned Logo" />
+            <h1 className="header__heading">{appTitle}</h1>
+          </div>
+
+          <button className="btn btn-large shareBtn">Share a fact</button>
+        </header>
+
+        <NewFactForm />
+
+        <main className="main">
+          <CategoryFilter />
+          <FactList />
+        </main>
+      </AppContainer>
+    </>
   );
+};
+
+const NewFactForm = () => {
+  return <form className="fact-form">Fact form</form>;
+};
+
+const CategoryFilter = () => {
+  return <aside className="sidebar">CategoryFilter</aside>;
+};
+
+const FactList = () => {
+  return <section className="fact-list">Facts List</section>;
 };
 
 export default App;
