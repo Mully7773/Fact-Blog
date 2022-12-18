c5291825d0a7ae346e1b53225b34f6d630a9fb00
Nick M <mully7773@gmail.com>
2022-12-17 22:55:00 -0600

    Export Sorter to its own component file and add guard clause to prevent error when clicking Choose vote type in sorter

--- src/components/Facts/FactList.js
+++ src/components/Facts/FactList.js
@@ -1,42 +1,5 @@
 import Fact from "./Fact";
-import supabase from "../../supabase";
-
-const Sorter = (props) => {
-  async function sortData(e) {
-    const currentVoteType = e.target.value;
-    // console.log(currentVoteType);
-
-    let sortQuery = supabase.from("facts").select("*");
-
-    if (props.filter !== "all") {
-      sortQuery = sortQuery.filter("category", "in", `("${props.filter}")`);
-    }
-
-    const { data: facts, error } = await sortQuery
-      // .filter("category", "in", '("technology")')
-
-      // .filter("category", "in", `("${props.filter}")`)
-      .order(currentVoteType, { ascending: false });
-
-    if (!error) {
-      props.setFacts(facts);
-    }
-  }
-  return (
-    <form className="sort-form">
-      <span className="sort-form__heading">Sort:</span>
-      <select
-        className="sort-form__select sort-form__select-u"
-        onChange={sortData}
-      >
-        <option value="">Choose vote type</option>
-        <option value="votesInteresting">Interesting</option>
-        <option value="votesMindblowing">Mindblowing</option>
-        <option value="votesFalse">Disputed</option>
-      </select>
-    </form>
-  );
-};
+import Sorter from "./Sorter";
 
 const FactList = (props) => {
   if (props.facts.length === 0) {
---
+++ src/components/Facts/Sorter.js
@@ -0,0 +1,40 @@
+import supabase from "../../supabase";
+
+const Sorter = (props) => {
+  async function sortData(e) {
+    const currentVoteType = e.target.value;
+    // Prevent error from clicking on 'Choose vote type'
+    if (!currentVoteType) return;
+
+    let sortQuery = supabase.from("facts").select("*");
+
+    // Conditionally render category filtered sort when 'all' is not the current state snapshot (add .filter to the chain of methods)
+    if (props.filter !== "all") {
+      sortQuery = sortQuery.filter("category", "in", `("${props.filter}")`);
+    }
+
+    const { data: facts, error } = await sortQuery.order(currentVoteType, {
+      ascending: false,
+    });
+
+    if (!error) {
+      props.setFacts(facts);
+    }
+  }
+  return (
+    <form className="sort-form">
+      <span className="sort-form__heading">Sort:</span>
+      <select
+        className="sort-form__select sort-form__select-u"
+        onChange={sortData}
+      >
+        <option value="">Choose vote type</option>
+        <option value="votesInteresting">Interesting</option>
+        <option value="votesMindblowing">Mindblowing</option>
+        <option value="votesFalse">Disputed</option>
+      </select>
+    </form>
+  );
+};
+
+export default Sorter;
